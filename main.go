d5d7ead1fbc2e02cb1ebf978e3e72626deef6680
Joshua Glass <joshuag1000@outlook.com>
2020-08-23 00:58:55 +0100

    Adjusted the program to use my GoEssentials Package

--- main.go
+++ main.go
@@ -4,13 +4,12 @@ import (
 	"bufio"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
-	"runtime"
 	"strconv"
+
+	JG "github.com/joshuag1000/GoEssentials"
 )
 
 var MainSiteURL string
@@ -41,7 +40,7 @@ func main() {
 	MainSiteURL = "127.0.0.1"
 	//SitePort = "8443"
 	//NonHttpsPort = "8080"
-	fmt.Println("The server ip is: " + GetServerIp(0))
+	fmt.Println("The server ip is: " + JG.GetServerIP(0))
 	//initPages
 	initPages()
 	//readCSV("file.de")
@@ -49,12 +48,12 @@ func main() {
 	go func() {
 		if _, err := os.Stat(ExecPath + "/HTTPS-key/server.crt"); os.IsNotExist(err) {
 			fmt.Printf("server.crt does not exist. HTTPS NOT STARTED\n")
-			openbrowser("http://" + MainSiteURL + ":" + NonHttpsPort + "/")
+			JG.OpenBrowser("http://" + MainSiteURL + ":" + NonHttpsPort + "/")
 		} else if _, err := os.Stat(ExecPath + "/HTTPS-key/server.key"); os.IsNotExist(err) {
 			fmt.Printf("server.key does not exist. HTTPS NOT STARTED\n")
-			openbrowser("http://" + MainSiteURL + ":" + NonHttpsPort + "/")
+			JG.OpenBrowser("http://" + MainSiteURL + ":" + NonHttpsPort + "/")
 		} else {
-			openbrowser("https://" + MainSiteURL + ":" + SitePort + "/")
+			JG.OpenBrowser("https://" + MainSiteURL + ":" + SitePort + "/")
 			// begin https server
 			err_https := http.ListenAndServeTLS(":"+SitePort, ExecPath+"/HTTPS-key/server.crt", ExecPath+"/HTTPS-key/server.key", nil)
 			if err_https != nil {
@@ -67,46 +66,6 @@ func main() {
 	err_http := http.ListenAndServe(":"+NonHttpsPort, nil)
 	if err_http != nil {
 		log.Fatal("Web server (HTTP): ", err_http)
-	}
-}
-
-func GetServerIp(ipNum int) string {
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
-		os.Exit(1)
-	}
-
-	var ips [255]string
-	var i int
-	i = 0
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To4() != nil {
-				ips[i] = ipnet.IP.String()
-				i = i + 1
-			}
-		}
-	}
-	return ips[ipNum]
-}
-
-func openbrowser(url string) {
-	if openBrowserOnLoad == true {
-		var err error
-		switch runtime.GOOS {
-		case "linux":
-			err = exec.Command("xdg-open", url).Start()
-		case "windows":
-			err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-		case "darwin":
-			err = exec.Command("open", url).Start()
-		default:
-			err = fmt.Errorf("unsupported platform")
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 }
 
