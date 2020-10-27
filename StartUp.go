01896802701c6ab0fed9fbd220a03ace9bb048c5
Joshua Glass <joshuag1000@outlook.com>
2020-10-27 15:11:13 +0000

    Moved a function to my essentials package
    This might break something but if it does ill fix it eventually.

--- StartUp.go
+++ StartUp.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"os"
 )
@@ -38,20 +37,4 @@ func check(e error) {
 	if e != nil {
 		log.Fatal(e)
 	}
-}
-
-// WriteToFile will print any string of text to a file safely by
-// checking for errors and syncing at the end.
-func WriteToFile(filename string, data string) error {
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.WriteString(file, data)
-	if err != nil {
-		return err
-	}
-	return file.Sync()
 }
